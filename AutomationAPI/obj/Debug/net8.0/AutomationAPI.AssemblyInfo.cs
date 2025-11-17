f3fef1570292d8badfcf01990a7ba51bc0550d0c
gnbabu <naresh.net2009@gmail.com>
2025-11-17 22:38:38 +0530

    Cleanup ignored files
    Cleanup ignored files

--- AutomationAPI/obj/Debug/net8.0/AutomationAPI.AssemblyInfo.cs
+++ AutomationAPI/obj/Debug/net8.0/AutomationAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutomationAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc06025b962e01cfd3bd6b88f26870dbf195959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6758de72e13b5b504d65dabe29e8d2a70e9105")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutomationAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutomationAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
